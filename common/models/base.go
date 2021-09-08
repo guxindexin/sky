9b70b9cb22af6961991f1cba09322cdedf4c9a90
lanyulei <fdevops@163.com>
2021-09-08 13:04:01 +0800

    feat: 配置逻辑删除。

--- common/models/base.go
+++ common/models/base.go
@@ -2,11 +2,13 @@ package models
 
 import (
 	"time"
+
+	"gorm.io/gorm"
 )
 
 type BaseModel struct {
-	Id        int        `gorm:"primary_key;column:id;type:serial;" json:"id" form:"id"`
-	CreatedAt time.Time  `gorm:"column:create_time" json:"create_time" form:"create_time"`
-	UpdatedAt time.Time  `gorm:"column:update_time" json:"update_time" form:"update_time"`
-	DeletedAt *time.Time `gorm:"column:delete_time" sql:"index" json:"-"`
+	Id        int            `gorm:"primary_key;column:id;type:serial;" json:"id" form:"id"`
+	CreatedAt time.Time      `gorm:"column:create_time" json:"create_time" form:"create_time"`
+	UpdatedAt time.Time      `gorm:"column:update_time" json:"update_time" form:"update_time"`
+	DeletedAt gorm.DeletedAt `gorm:"column:delete_time" sql:"index" json:"-"`
 }
